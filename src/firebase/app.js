396efde8c7702bbcdd3bf98e6a8056a1d4f95de9
NoahSignt <noahvass01@gmail.com>
2024-04-04 12:38:48 +1100

    CORE: Initial commit

---
+++ src/firebase/app.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhiCNJ-WyXjxS2sdP0VSSFgI54mGTFQSc",
+  authDomain: "workoutplanner-8ad52.firebaseapp.com",
+  projectId: "workoutplanner-8ad52",
+  storageBucket: "workoutplanner-8ad52.appspot.com",
+  messagingSenderId: "92221100906",
+  appId: "1:92221100906:web:00dda8c80ac0072c684b13"
+};
+
+const app = initializeApp(firebaseConfig);
