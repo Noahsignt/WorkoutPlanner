3a7c5dd62914d8868e18fda7d40091086f333c3e
NoahSignt <noahvass01@gmail.com>
2024-04-22 15:49:19 +1000

    auth-setup: simple login form complete

--- src/app/components/UserForm.tsx
+++ src/app/components/UserForm.tsx
@@ -4,6 +4,7 @@ import { signup, login } from "@/firebase/auth/login"
 import { FormEvent, useState } from "react";
 
 export default function UserForm() {
+    const [isLogin, setLogin] = useState(true);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -18,22 +19,57 @@ export default function UserForm() {
     }
 
     return (
-        <form className={styles['user-form']} onSubmit={submitLogin}>
-           <label htmlFor="email">Email:</label>
-           <input 
-              type="text" 
-              name="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-           />
-           <label htmlFor="password">Password:</label>
-           <input 
-              type="password" 
-              name="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-           />
-           <input type="submit" value="Log in"/>
-        </form>
+        isLogin ? 
+        <div className={styles['user-form']}>
+            <h1>Login</h1>
+            <form onSubmit={submitLogin}>
+                <div className={styles['user-form-bloc']}>
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="text" 
+                        name="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
+                <div className={styles['user-form-bloc']}>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
+                <input type="submit" value="Login"/>
+            </form>
+            <button className={styles['user-form-swapper']} onClick={() => setLogin(false)}>Register</button>
+        </div>
+        :
+        <div className={styles['user-form']}>
+            <h1>Register</h1>
+            <form onSubmit={submitSignUp}>
+                <div className={styles['user-form-bloc']}>
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                    type="text" 
+                    name="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
+                <div className={styles['user-form-bloc']}>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
+                <input type="submit" value="Register"/>
+            </form>
+            <button className={styles['user-form-swapper']} onClick={() => setLogin(true)}>Login</button>
+        </div>
     );
 }
